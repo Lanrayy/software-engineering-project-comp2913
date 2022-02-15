ff92066cc80a768b68f8e517c733c4e92d1f0170
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2022-02-15 19:59:15 +0000

    Added Login and Register Forms to forms.py file

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,33 @@
 from flask_wtf import Form
-from wtforms import TextField
-from wtforms.validators import DataRequired
 
-class TestForm(Form):
-    string1 = TextField('string1', validators=[DataRequired()])
+from wtforms import IntegerField
+from wtforms import StringField
+from wtforms import TextAreaField
+from wtforms import DateField
+from wtforms import SubmitField
+from wtforms import PasswordField
+from wtforms import BooleanField
+from wtforms import ValidationError
+
+from wtforms.validators import InputRequired, EqualTo, Length, Required, Email
+
+# sign up form
+class SignUpForm(Form):
+    name = StringField('name', validators=[InputRequired()])
+    username = StringField('username', validators=[InputRequired(), Length(min=2, max=20)])
+    email = StringField('email', validators=[InputRequired(), Email()])
+    password = PasswordField('password1', validators=[InputRequired()])
+    password2 = PasswordField('password2', validators=[EqualTo('password'), InputRequired()]) #makes sure password1 equals password2
+    
+    # validating that the username is unique
+    # def validate_username(self, username):
+    #     user = models.Users.query.filter_by(username=username.data).first()
+    #     # if a username if found raise an error
+    #     if user:
+    #         raise ValidationError('Username taken! Please choose a different username')
+
+# login form
+class LoginForm(Form):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
+    remember = BooleanField('Remember Me')
