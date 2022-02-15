5baae8ffa5e02aa1a7d50207e28a4af570250969
Alex Cheong <sc20a2c@leeds.ac.uk>
2022-02-15 18:53:34 +0000

    added test form and create python files for cconfig and forms

--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
+from flask import render_template, flash
 from app import app
+from .forms import TestForm
 
 @app.route('/')
 def index():
-    return "Hello World!"
+        return render_template('home.html',
+                               title='Home')
+
+@app.route('/testform', methods=['GET', 'POST'])
+def testform():
+    form = TestForm()
+    if form.validate_on_submit():
+        flash('Succesfully received form data. %s'%(form.string1.data))
+    return render_template('testform.html',
+                           title='Test Form',
+                           form=form)
---
+++ config.py
@@ -0,0 +1,4 @@
+WTF_CSRF_ENABLED = True
+
+# You should change this
+SECRET_KEY = 'another-secret-secret'
