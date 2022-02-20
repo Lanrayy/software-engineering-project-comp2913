920d51a35f2749cdf5725676a9b8daa6cf795948
Alex Cheong <sc20a2c@leeds.ac.uk>
2022-02-20 17:30:53 +0000

    connected register & login with models.user

--- app/views.py
+++ app/views.py
@@ -1,13 +1,9 @@
 from flask import render_template, flash
-from app import app
+from app import app, db, bcrypt, models
 from .forms import LoginForm, SignUpForm
 from flask import request, redirect, url_for, abort, make_response
-#from .models import User
-from app import app, db
-#from .forms import RegistrationForm, LoginForm
 from flask_login import login_user, current_user, logout_user, login_required
 import os
-from flask_bcrypt import Bcrypt
 
 @app.route('/')
 def index():
@@ -18,14 +14,14 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:   # if current user is logged in
-        return redirect('home.html')
+        return redirect(url_for('index'))
 
     form = SignUpForm()
 
     # if form is submitted
     if form.validate_on_submit():
 
-        flash('Succesfully received form data. %s %s %s %s %s'%(form.name.data, form.username.data, form.email.data, form.password1.data, form.password2.data))
+        flash('Succesfully received form data. %s %s %s %s'%(form.name.data, form.email.data, form.password1.data, form.password2.data))
 
         # set user = current user
 
@@ -33,12 +29,12 @@ def register():
         hashed_password= bcrypt.generate_password_hash(form.password1.data)
 
 
-        user = User(password = hashed_password, email = form.email.data , name  = form.name.data)
+        u = models.user(password = hashed_password, email = form.email.data , status="normal" , name = form.name.data)
 
-        db.session.add(user)    # add user to db
+        db.session.add(u)    # add user to db
         db.session.commit()     # commit user to db
         flash('Account Created! Please Log In', 'success')
-            
+
         return redirect(url_for('user_login'))   # redirect to login page
 
     else:
@@ -50,8 +46,8 @@ def register():
 @app.route('/user_login', methods=['GET', 'POST'])
 def user_login():
     if current_user.is_authenticated:   # if current user is logged in
-        return redirect (url_for('home'))
-    
+        return redirect (url_for('index'))
+
     form = LoginForm()
 
     # if form is submitted
@@ -60,18 +56,18 @@ def user_login():
         ##flash('Succesfully received form data. %s %s %s'%(form.username.data, form.password.data, form.remember.data))
 
         # get first instance of user in db
-        user = User.query.filter_by(username = form.username.data).first()
+        u = models.user.query.filter_by(email = form.email.data).first()
 
         # check username and password
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password):
-                login_user(user)
+        if u:
+            if bcrypt.check_password_hash(u.password, form.password.data):
+                login_user(u)
                 flash('Login Successful!', 'success')
-                return redirect('user_login.html')
+                return redirect(url_for('user_login'))
             else:
                 flash('Wrong Password!')
         else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
+            flash('Login unsuccessful. Please check email and password', 'danger')
 
 
     return render_template('user_login.html',
@@ -80,10 +76,10 @@ def user_login():
 
 
 # only logout if user is logged in
-@app.route ("/logout")
+@app.route ('/logout')
 @login_required
 def logout():
     logout_user()
     flash('Logout Successful!', 'info')
     # redirect to home page
-    return redirect('')
+    return redirect(url_for('index'))
