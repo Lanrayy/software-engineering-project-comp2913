65010c417fafdba7a890b6e6e95ce2ac92d62337
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2022-02-22 17:58:24 +0000

    Added form for feedback page

--- app/forms.py
+++ app/forms.py
@@ -49,3 +49,7 @@ class BookingForm(FlaskForm):
     location_id = SelectField('location_id', choices=[('1', 'Trinity Centre'), ('2', 'Train Station'), ('3', 'Merrion Centre'),
         ('4', 'LRI Hospital'), ('5', 'UoL Edge Sports Centre')])
     hire_period = SelectField('hire_period', choices = [('1', '1 Hour'), ('2', '4 Hours'), ('3', '1 Day'), ('4', '1 Week')])
+
+# feedback form
+class FeedbackForm(FlaskForm):
+    feedback = TextAreaField('feedback')
